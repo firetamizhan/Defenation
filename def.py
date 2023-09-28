aa3d00dac3cf5306e45ae1c37fe72882da3e5861
kasir <kasiramsureshm719@gmail.com>
2023-09-28 13:41:17 +0530

    def.py added

---
+++ def.py
@@ -0,0 +1,24 @@
+def CEO(name):#Harks Corp #person 2
+    print("CEO-Suresh")
+    return name
+
+def Add():
+    print("Add")
+    print("US")
+    return ""
+
+def company_landingPage():#person 1
+    name = "Harks Corp"
+    print("CompanyName", CEO(name))#person2
+    print("______________")
+    print("This company is like product based MNC")
+    print("______________")
+    Add()
+    return ""
+
+#planning start a company need to create a website 
+#Bio or landingPage
+
+#company_landingPage()
+
+company_landingPage()#call
